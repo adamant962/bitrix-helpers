f441cee0092715a9288ece06e2b988797bc211a5
Tumen <t.dugarov@tanais.ru>
2022-10-28 13:37:52 +0300

    rename namespace and add arselect params in methods

--- src/IblockRepositoryInterface.php
+++ src/IblockRepositoryInterface.php
@@ -2,14 +2,14 @@
 
 declare(strict_types=1);
 
-namespace Tanais;
+namespace BitrixHelp;
 
 interface IblockRepositoryInterface
 {
     /**
      * @param int $element_id идентификатор элемента
      */
-    public function getElementById(int $element_id): IblockElementInterface;
+    public function getElementById(int $element_id, array $ar_select): IblockElementInterface;
 
-    public function getSectionById(int $element_id): IblockSectionInterface;
+    public function getSectionById(int $element_id, array $ar_select): IblockSectionInterface;
 }
--- src/IblockSectionInterface.php
+++ src/IblockSectionInterface.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Tanais;
+namespace BitrixHelp;
 
 interface IblockSectionInterface
 {
